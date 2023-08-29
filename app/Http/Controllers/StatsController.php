3df0e540bcc5de22041c7e35c31e1a6e8c3ef35a
Zoe <zoewang8860@gmail.com>
2023-08-29 14:46:12 -0700

    Implement getFollowerStats method

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -2,14 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use stdClass;
 
 class StatsController extends Controller
 {
     public function getFollowerStats(Request $request) {
+        if(!$request->user_id) {
+            return response()->json('Invalid request', 400);
+        }
+        // by default, query follower stats for past 30 days
+        $days = $request->days ? $request->days : 30;
         // query total amount of follower gained in a time frame
-        return response()->json('not implemented');
+        $followerCount = DB::table('followers')
+            ->where('user_id', $request->user_id)
+            ->where('followed_at', '>=', Carbon::now()->subDays($days)->toDateTimeString())
+            ->count();
+        $response = new stdClass();
+        $response->followerCount = $followerCount;
+        $response->days = $days;
+        $response->user_id = $request->user_id;
+        return response()->json($response);
     }
 
     public function getSaleItemStats(Request $request) {
