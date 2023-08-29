a4f93a5191bfc33f059cba45264faf23442e4517
Zoe <zoewang8860@gmail.com>
2023-08-29 15:27:57 -0700

    Implement getRevenueStats endpoints

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -36,9 +36,12 @@ class StatsController extends Controller
         if($item_number > $max_item_number) {
             $item_number = $max_item_number;
         }
+        $days = $request->days ? $request->days : 30;
+
         $itemList = DB::table('merch_sales')
             -> select('item_name', DB::raw('SUM(quantity * price) as "revenue"'))
             -> where('merch_id', $request->user_id)
+            -> where('timestamp', '>=', Carbon::now()->subDays($days)->toDateTimeString())
             -> groupBy('item_name')
             -> orderBy('revenue', 'desc')
             -> limit($item_number)
@@ -48,6 +51,28 @@ class StatsController extends Controller
     }
 
     public function getRevenueStats(Request $request) {
-        return response()->json('not implemented');
+        if(!$request->user_id) {
+            return response()->json('Invalid request', 400);
+        }
+        $days = $request->days ? $request->days : 30;
+
+        $donation_revenue = DB::table('donations')
+            -> select(DB::raw('"donation" as eventType'), DB::raw('SUM(amount) as "revenue"'))
+            -> where('user_id', $request->user_id)
+            -> where('timestamp', '>=', Carbon::now()->subDays($days)->toDateTimeString());
+
+        $subscription_revenue = DB::table('subscribers')
+            -> select(DB::raw('"subscription" as eventType'), 
+            DB::raw('SUM((CASE WHEN tier = 1 THEN 5 WHEN tier = 2 THEN 10 WHEN tier = 3 THEN 15 ELSE 0 END)) AS "Revenue"'))
+            -> where('user_id', $request->user_id)
+            -> where('subscribed_at', '>=', Carbon::now()->subDays($days)->toDateTimeString());
+        
+        $merch_sale_revenue = DB::table('merch_sales')
+        -> select(DB::raw('"merch_sale" as eventType'), DB::raw('SUM(quantity * price) as "revenue"'))
+        -> where('merch_id', $request->user_id)
+        -> where('timestamp', '>=', Carbon::now()->subDays($days)->toDateTimeString());
+
+        $revenueStats = $donation_revenue->union($subscription_revenue)->union($merch_sale_revenue)->get();
+        return response()->json($revenueStats);
     }
 }
